6478b9aefdeabbb18771a01058eb9c2da1885a90
Mikey <a0898408984@gmail.com>
2021-07-04 21:19:53 +0800

    feat:

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from directGraph import DirectedGraph
 from utilities import *
 from utilities2 import *
 from utilities1415 import *
-import time
 import json
+import pickle
 
-def read(graph, filename):
+
+def read(graph, filename='data.json'):
     with open(filename, 'r') as obj:
         data = json.load(obj)
     add_people(graph, data)
 
-def read_relation(graph):
-    filename = input("please input relation file name: ")
+def read_relation(graph,filename ='relationship.json'):
     with open(filename, 'r') as obj:
         datas = json.load(obj)
     for data in datas:
@@ -30,11 +30,19 @@ def show_personal_status(graph):
     for key in keys:
         print("name: " + str(graph._vertices_list[key].value['name']), end='')
         for f in fields:
+            tmp = str(graph._vertices_list[key].value[f])
+            tmp = tmp if len(tmp) != 0 else 'empty'
             print(", " + str(f) + ": " + \
-                    str(graph._vertices_list[key].value[f]), end='')
+                    tmp, end='')
         print()
     print("show personal status complete")
 
+def show_who_infected(graph):
+    print("print people who get infected: ")
+    tmp = show_infected(graph,None)
+    tmp = None if len(tmp) == 0 else tmp
+    print(tmp)
+    
 def add_single_relation(graph):
     print("please input single relation")
     print("format: [3, 6, 7, 8], 0")
@@ -67,35 +75,119 @@ def find_contacted_main(graph):
     print("format: [0, 2, 3, 5, 6]")
     people = eval(input(""))
     print(find_contacted(graph, people))
-    print("find contacted conplete")
+    print("find contacted complete")
+
+def find_by(graph):
+    c = input('please input find by(name = n, job = j, age = a, gender = g, infected-time = i) = ').lower().strip()[0]
+    d = {'n':'name','j':'job','a':'age','g':'gender','i':'infected-time'}
+    try:
+        d[c]
+    except:
+        return
+    try:
+        value = input('input a value using to search data {} = '.format(d[c])).lower().strip()
+        print(graph.get_search_list(d[c],value))
+    except:
+        print('find nothing')
+def save_data(graph):
+    try:
+        file = open(b"fileGraph.p","wb")
+        pickle.dump(graph,file)
+        print('save data successful!')
+    except:
+        print('save data -> exit(): unsuccessful')
+def load_data():
+    try:
+        file = open("fileGraph.p",'rb')
+        print('load data successful!')
+        return pickle.load(file)
+    except:
+        print('load data -> exit(): unsuccessful')
+
 
 if __name__ == '__main__':
     print("start program")
-    graph = DirectedGraph()
+    print(graph)
     while True:
-        command = input("please input command: ")
-        if command == 'exit':
+        command = input("please input command: ").lower().strip()
+        command = command.split(' ')
+        tmp = []
+        for x in command:
+            if len(x) != 0:
+                tmp.append(x)
+        command = ' '.join(tmp)
+        if command == 'exit' or command == 'r0':
             print("stop program")
             break
         print("command you input: " + command)
-        if command == 'read people':
-            filename = input("please input people file name: ")
-            read(graph, filename)
+        if command == 'read by default' or  command == 'r1':
+            read(graph)
             print("read people data complete!")
-        elif command == 'read relation':
             read_relation(graph)
-        elif command == 'show status':
+            continue
+        if command == 'save' or  command == 'r+':
+            save_data(graph)
+            continue
+        if command == 'load' or  command == 'r-':
+            graph = load_data()
+            continue
+        elif command == 'read people'or  command == 'r2':
+            try:
+                filename = input("please input people file name: ")
+                read(graph, filename)
+                print("read people data complete!")
+            except:
+                print('read people -> exit(): wrong file name')
+            continue
+        elif command == 'read relation' or  command == 'r3':
+            try:
+                filename = input("please input people file name: ")
+                read_relation(graph,filename)
+            except:
+                print('read relation -> exit(): wrong file name')
+            continue
+        elif command == 'show status' or  command == 'r4':
             show_vertex_status(graph, 0, 10000, False)
-        elif command == 'show personal status':
-            show_personal_status(graph)
-        elif command == 'add single relation':
-            add_single_relation(graph)
-        elif command == 'add infected':
-            add_infected(graph)
-        elif command == 'add tmp relation':
-            add_tmp_relation_main(graph)
-        elif command == 'find contacted':
-            find_contacted_main(graph)
+            continue
+        elif command == 'show personal status' or  command == 'r5':
+            try:
+                show_personal_status(graph)
+            except:
+                print('show personal status -> exit() : wrong in key')
+            continue
+        elif command == 'show who infected' or  command == 'r6' :
+            show_who_infected(graph)
+            continue
+        elif command == 'add single relation' or  command == 'r7' :
+            try:
+                add_single_relation(graph)
+            except:
+                print('add single relation -> exit()')
+            continue
+        elif command == 'add infected' or  command == 'r8':
+            try:
+                add_infected(graph)
+            except:
+                print('add infected -> exit()')            
+            continue
+        elif command == 'add tmp relation' or  command == 'r9':
+            try:
+                add_tmp_relation_main(graph)
+            except:
+                print('add tmp relation -> exit()')
+            continue
+        elif command == 'find contacted' or  command == 'r10':
+            try:
+                find_contacted_main(graph)
+            except:
+                print('find contacted -> exit()')
+            continue
+        elif command == 'find people by' or  command == 'r11':
+            try:
+                find_by(graph)
+            except:
+                print('find people by -> exit()')
+            continue
         else:
             print("invalid command")
 
